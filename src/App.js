b9ac26988330c6fdadcd76b474c12c1c1a7f6332
Kevin Lin <kevinlin.vxd@gmail.com>
2021-10-09 10:47:29 -0500

    Finished Wiki Seeker - May switch CSS

--- src/App.js
+++ src/App.js
@@ -11,15 +11,14 @@ function App() {
 
     const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${search}`
     const response = await fetch(endpoint)
-
-    console.log(response)
+    //console.log(response)
 
     if (!response.ok) {
       throw Error(response.statusText)
     }
 
     const json = await response.json()
-    console.log(json)
+    //console.log(json)
 
     setResults(json.query.search)
     setSearchInfo(json.query.searchinfo)
@@ -35,22 +34,26 @@ function App() {
             value={search}
             onChange={e => setSearch(e.target.value)}
           />
-          {searchInfo.totalhits ? (
-            <p> Search Results: {searchInfo.totalhits} </p>
-          ) : (
-            ""
-          )}
         </form>
+        {searchInfo.totalhits ? (
+          <p> Search Results: {searchInfo.totalhits} </p>
+        ) : (
+          ""
+        )}
       </header>
       <div className="results">
-        <div className="result">
-          <h3> Title Goes Here</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione,
-            cupiditate.
-          </p>
-          <a href="#">Read More</a>
-        </div>
+        {results.map((result, i) => {
+          const url = `https://en.wikipedia.org/?curid=${result.pageid}`
+          return (
+            <div className="result" key={i}>
+              <h3> {result.title} </h3>
+              <p dangerouslySetInnerHTML={{ __html: result.snippet }}></p>
+              <a href={url} target="_blank" rel="noreferrer">
+                Read More
+              </a>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
